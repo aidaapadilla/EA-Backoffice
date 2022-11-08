e5c290b03e582df149ed22f114584a00e13beaf8
Aida Padilla <aida.padilla.gonzalez.aiko@gmail.com>
2022-11-08 22:30:11 +0100

    roc

--- src/app/components/create-parking2/create-parking2.component.ts
+++ src/app/components/create-parking2/create-parking2.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { User } from 'src/app/interfaces/user.interface';
 import { Validators } from '@angular/forms';
+import { Parking } from 'src/app/interfaces/parking';
 
 @Component({
   selector: 'app-create-parking2',
@@ -9,10 +10,27 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./create-parking2.component.css']
 })
 export class CreateParking2Component implements OnInit {
-
+  parking!: Parking;
+    @Output() addParking = new EventEmitter<Parking>();
+    parkingForm = new FormGroup({
+        //country: new FormControl('', Validators.required), //En parking.ts he de posar el que falta
+        //city: new FormControl('', [Validators.required]),
+        //street: new FormControl('', [Validators.required]), // Validators.min(6)
+        //spotNumber: new FormControl('', [Validators.required]),
+        type: new FormControl('', [Validators.required]),
+        price: new FormControl('', [Validators.required]), //Number
+        size: new FormControl('', [Validators.required]),
+        difficulty: new FormControl('', [Validators.required, Validators.max(2)]), //Number
+        score: new FormControl('', [Validators.required]) //Number
+    })
   constructor() { }
 
   ngOnInit(): void { //Podem guardar el usuari complet al token
   } 
+  onSubmit() {// Buscar com fer la conversio
+    //this.parking = new Parking(this.parkingForm.value.type, this.parkingForm.value.price,this.parkingForm.value.size,this.parkingForm.value.difficulty,this.parkingForm.value.score) 
+    //this.parking = <Parking>this.parkingForm.value
+    this.addParking.emit(this.parking);
+  }
 
 }
