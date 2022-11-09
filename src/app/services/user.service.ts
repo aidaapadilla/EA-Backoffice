a3f3bd96a2818729b317b17cd88fcfd8635faff3
Alba Onrubia <alba.onrubia2@estudiantat.upc.edu>
2022-11-09 14:07:37 +0100

    Falla update

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,17 +10,22 @@ export class UsersService {
   private apiURL = 'http://localhost:5432/api/users/';
   constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<User[]> { //BARTO
+  getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiURL);
   }
-  delete(user: string): Observable<User> { //BARTO
+  delete(user: string): Observable<User> {
     return this.http.delete<User>(this.apiURL + 'delete/' + user)
 
   }
 
-  addUser(user: User): Observable<User> { //BARTO
+  addUser(user: User): Observable<User> {
     return this.http.post<User>(this.apiURL + 'register/', user)
+  }
 
+  updateUser(user: User, id: string): Observable<User> {
+    console.log(user);
+    console.log(id);
+    return this.http.put<User>(this.apiURL + 'update/' + id, user)
   }
 
   deleteUser(id: string): Observable<User> { 
