50859c6cd71023f26bb60424988be69408141b32
Ferran <ferran.saigi@estudiantat.upc.edu>
2022-11-09 18:47:19 +0100

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './components/login/login.component';
 import { ParkingsComponent } from './components/parkings/parkings.component';
 import { CreateParking2Component} from './components/create-parking2/create-parking2.component'
 
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: "parkings",
     component: ParkingsComponent
+  },
+  {
+    path: "user",
+    component: LoginComponent
   },
   {
     path: "parking",
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,18 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient,HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../interfaces/user.interface'
+import {BehaviorSubject, Observable} from 'rxjs';
+import { LogIn } from '../interfaces/logIn.interface';
+import {User} from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+
+  user!: User;
+  message!: String;
+  private userSource = new BehaviorSubject(this.user);
+  currentUser = this.userSource.asObservable();
   private apiURL = 'http://localhost:5432/api/users/';
   constructor(private http: HttpClient) { }
 
@@ -18,12 +24,21 @@ export class UsersService {
 
   }
 
+  logIn(userData:LogIn): Observable<HttpResponse<User>>{
+    return this.http.post<User>(this.apiURL + 'login/', userData, {observe: 'response'})
+  }
+
+  newUserLogged(user: User) {
+    //console.log("user a service: " + JSON.stringify(user));
+    this.userSource.next(user);
+  }
+
   addUser(user: User): Observable<User> { //BARTO
     return this.http.post<User>(this.apiURL + 'register/', user)
 
   }
 
-  deleteUser(id: string): Observable<User> { 
+  deleteUser(id: string): Observable<User> {
     return this.http.delete<User>(this.apiURL + '/delete/' + id);
   }
 }
